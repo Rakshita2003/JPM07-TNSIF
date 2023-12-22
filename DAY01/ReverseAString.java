c18fca4392fe1025b3b6bef7119d8f570c1f7a32
Rakshita <rakshitagowda22@gmail.com>
2023-12-22 07:53:22 +0530

    Uploading Day1 Programs

---
+++ DAY01/ReverseAString.java
@@ -0,0 +1,18 @@
+// Question 3: Reverse a String:
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        String originalStr = "Hello";
+        String reversedStr = "";
+        System.out.println("Original string: " + originalStr);
+
+
+        for (int i = 0; i < originalStr.length(); i++) {
+            reversedStr = originalStr.charAt(i) + reversedStr;
+        }
+
+
+        System.out.println("Reversed string: " + reversedStr);
+    }
+
+}
